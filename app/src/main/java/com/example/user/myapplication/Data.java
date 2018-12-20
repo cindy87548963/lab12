005a1ee40f6eb427b5bc667e2e6ac5ef36870e75
cindy87548963 <cindy87548963@gmail.com>
2018-12-20 15:04:51 +0800

    first commit

---
+++ app/src/main/java/com/example/user/myapplication/Data.java
@@ -0,0 +1,14 @@
+package com.example.user.myapplication;
+
+import javax.xml.transform.Result;
+
+public class Data {
+    Result result ;
+    class Result{
+        Results[] results;
+        class  Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
